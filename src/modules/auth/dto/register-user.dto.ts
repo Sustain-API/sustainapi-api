7759f2a2e0ae2a01b95c8603a1bebd3983019691
theijhay <olawaleisaacjohn@gmail.com>
2024-10-26 17:49:08 +0100

    update register dto

--- src/modules/auth/dto/register-user.dto.ts
+++ src/modules/auth/dto/register-user.dto.ts
@@ -2,21 +2,21 @@ import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from '@nestjs/cl
 import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterUserDto {
-  @ApiProperty({ description: 'The full name of the user' })
+  @ApiProperty({ example: 'Isaac John', description: 'The full name of the user' })
   @IsNotEmpty()
   @IsString()
   full_name: string;
 
-  @ApiProperty({ description: 'The email of the user' })
+  @ApiProperty({ example: 'isaacjohn@gmail.com', description: 'The email of the user' })
   @IsEmail()
   email: string;
 
-  @ApiProperty({ description: 'The password of the user', minLength: 6 })
+  @ApiProperty({ example: 'password123', description: 'The password of the user', minLength: 6 })
   @IsNotEmpty()
   @MinLength(6)
   password: string;
 
-  @ApiProperty({ description: 'The wallet address of the user (optional)', required: false })
+  @ApiProperty({ example: '0xB32c3edcA357d1B494a71F277d054A22A8d6BC2b', description: 'The wallet address of the user (optional)', required: false })
   @IsOptional()
   @IsString()
   wallet_address?: string;
